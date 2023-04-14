fd1c78c3d6137978de2c6b9055047a9980eda195
Caio Bastos <caio.bastos@desbravador.com.br>
2023-04-14 14:09:36 -0300

    crawler funcional

--- go.mod
+++ go.mod
@@ -3,31 +3,6 @@ module github.com/caiovillardo/crawler
 go 1.23
 
 require (
-	github.com/go-kit/kit v0.12.0
-	github.com/golang/protobuf v1.5.3
-	github.com/jinzhu/gorm v1.9.16
-	github.com/joho/godotenv v1.5.1
-	github.com/satori/go.uuid v1.2.0
-	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272
-	google.golang.org/grpc v1.53.0
-	google.golang.org/protobuf v1.30.0
-)
-
-require (
-	github.com/PuerkitoBio/goquery v1.5.1 // indirect
-	github.com/antchfx/htmlquery v1.2.3 // indirect
-	github.com/antchfx/xmlquery v1.2.4 // indirect
-	github.com/go-kit/log v0.2.1 // indirect
-	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230320184635-7606e756e683 // indirect
+	github.com/lib/pq v1.10.7
+	golang.org/x/time v0.3.0
 )
--- main.go
+++ main.go
@@ -1,54 +1,121 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"net/url"
-	"time"
+
+	_ "github.com/lib/pq"
+	"golang.org/x/time/rate"
 )
 
-type MovideskTicket struct {
-	ID          int    `json:"id"`
-	Subject     string `json:"subject"`
-	Description string `json:"description"`
+const baseUrl = "https://api.movidesk.com/public/v1"
+const apiKey = "d495699b-f706-4ca6-9149-51012215ace3"
+const route = "/services"
+
+func fetchData() {
+	limiter := rate.NewLimiter(rate.Limit(6), 1) // Limite de 6 requisições por minuto
+	url := fmt.Sprintf("%s%s", baseUrl, route)
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("token", apiKey)
+
+	for {
+		err = limiter.Wait(req.Context()) // Espera o limite de requisições
+		if err != nil {
+			panic(err)
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("Erro na requisição: %s\n", resp.Status)
+			continue
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		err = ioutil.WriteFile("tickets.json", body, 0644)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Println("Os dados foram salvos com sucesso!")
+		break
+	}
+
+	const (
+		host     = "localhost"
+		port     = 5432
+		user     = "postgres"
+		password = "password"
+		dbname   = "mydatabase"
+	)
+
+	type Ticket struct {
+		Id                   int    `json:"id"`
+		Title                string `json:"title"`
+		Status               string `json:"status"`
+		Priority             string `json:"priority"`
+		Name                 string `json:"name"`
+		Description          string `json:"description"`
+		ParentServiceId      int    `json:"parentServiceId"`
+		ServiceForTicketType int    `json:"serviceForTicketType"`
+		IsVisible            int    `json:"isVisible"`
+		AllowSelection       int    `json:"allowSelection"`
+		AllowFinishTicket    bool   `json:"allowFinishTicket"`
+		IsActive             bool   `json:"isActive"`
+		AutomationMacro      string `json:"automationMacro"`
+		DefaultCategory      string `json:"defaultCategory"`
+		DefaultUrgency       string `json:"defaultUrgency"`
+		AllowAllCategories   bool   `json:"allowAllCategories"`
+	}
+
+	var ticketsJs []Ticket
+
+	jsonData, err := ioutil.ReadFile("tickets.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(jsonData, &ticketsJs)
+	if err != nil {
+		panic(err)
+	}
+
+	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	for _, ticket := range ticketsJs {
+		sqlStatement := `
+            INSERT INTO tickets (id, title, status, priority)
+            VALUES ($1, $2, $3, $4)`
+		_, err = db.Exec(sqlStatement, ticket.Id,
+			ticket.Title, ticket.Status, ticket.Priority)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func main() {
-	baseURL := "https://desbravador.movidesk.com/public/v1."
-	companyName := "desbravador"
-	apiToken := "d495699b-f706-4ca6-9149-51012215ace3"
-
-	client := &http.Client{Timeout: 10 * time.Second}
-
-	endpoint := fmt.Sprintf("%s/%s/search/ticket", baseURL, companyName)
-
-	values := url.Values{}
-	values.Add("$select", "id,subject,description")
-	values.Add("$top", "50")
-
-	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiToken))
-	req.URL.RawQuery = values.Encode()
-
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	var tickets []MovideskTicket
-	err = json.NewDecoder(resp.Body).Decode(&tickets)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, ticket := range tickets {
-		fmt.Printf("Ticket ID: %d\n", ticket.ID)
-		fmt.Printf("Subject: %s\n", ticket.Subject)
-		fmt.Printf("Description: %s\n\n", ticket.Description)
-	}
+	fetchData()
 }
